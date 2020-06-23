7f7934bbeaffddc493d881521813b021c8bac687
gymgym <naoki.hanzawa.pub@gmail.com>
2020-06-23 17:42:54 +0900

    Configure Presentation Layer for Dependency Container

---
+++ CleanArch/CleanArch.Infra.IoC/DenpendencyContainer.cs
@@ -0,0 +1,28 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DenpendencyContainer
+    {
+        /// <summary>
+        /// DIに関するオブジェクトをまとめる。
+        /// 具体的には、全てのプロジェクトの依存関係をIServiceCollectionに追加する
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ReigsterServices(IServiceCollection services) 
+        {
+            //Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            //Infra.Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
