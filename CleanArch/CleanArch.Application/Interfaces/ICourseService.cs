f8820579c77e2587701b5474ac3a494814f5df91
gymgym <naoki.hanzawa.pub@gmail.com>
2020-06-23 11:32:29 +0900

    Commit Implementation of Service and Repository GetCourses Method

--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -8,6 +8,6 @@ namespace CleanArch.Application.Interfaces
 {
     public interface ICourseService
     {
-        IEnumerable<CouseViewModel> GetCourses();
+        CouseViewModel GetCourses();
     }
 }
