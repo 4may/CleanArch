caffe0ef735a15eb4e65d20e207e61ca24ecdce3
gymgym <naoki.hanzawa.pub@gmail.com>
2020-06-24 17:33:29 +0900

    create時エラー オブジェクト名Coursesが無効です

--- CleanArch/CleanArch.Api/Startup.cs
+++ CleanArch/CleanArch.Api/Startup.cs
@@ -2,14 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
+using CleanArch.Mvc.Data;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CleanArch.Api
 {
@@ -25,7 +32,24 @@ namespace CleanArch.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //’Ç‰Á
+            services.AddDbContext<UniversityDBContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("UniversityIdentityDBConnection")));
+
             services.AddControllers();
+
+            //’Ç‰Á
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "University Api", Version = "v1" });
+            });
+
+            //’Ç‰Á
+            services.AddMediatR(typeof(Startup));
+
+            //’Ç‰Á
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +62,12 @@ namespace CleanArch.Api
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "University Api V1");
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -46,6 +76,15 @@ namespace CleanArch.Api
             {
                 endpoints.MapControllers();
             });
+        }
+
+        /// <summary>
+        /// ’Ç‰Á
+        /// </summary>
+        /// <param name="services"></param>
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DenpendencyContainer.ReigsterServices(services);
         }
     }
 }
