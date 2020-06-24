02c9e83d72201aba84ef7b6b80a1179e6e106f94
gymgym <naoki.hanzawa.pub@gmail.com>
2020-06-24 15:45:30 +0900

    Commands. CommandHandlers. Updated Course Repository, Cleaned up

---
+++ CleanArch/CleanArch.Domain/CommandHandlers/CourseCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using CleanArch.Domain.Commands;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.CommandHandlers
+{
+    /// <summary>
+    /// CreateCourseCommandを受け取って、DBにデータを新規追加するクラス
+    /// DB処理をするため、Repositoryをコンストラクタで受け取る必要がある
+    /// </summary>
+    public class CourseCommandHandler : IRequestHandler<CreateCourseCommand, bool>
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseCommandHandler(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public Task<bool> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+        {
+            //新規作成するコースを定義
+            var course = new Course()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl
+            };
+
+            //DBにデータを追加
+            _courseRepository.Add(course);
+
+            return Task.FromResult(true);
+        }
+    }
+}
--- CleanArch/CleanArch.Infra.IoC/DenpendencyContainer.cs
+++ CleanArch/CleanArch.Infra.IoC/DenpendencyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Bus;
+using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.Data.Repository;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -14,15 +20,24 @@ namespace CleanArch.Infra.IoC
         /// <summary>
         /// DIに関するオブジェクトをまとめる。
         /// 具体的には、全てのプロジェクトの依存関係をIServiceCollectionに追加する
+        /// Interfaceと、そのInterfaceの実装クラスの二つを語パラメータとして渡す。
         /// </summary>
         /// <param name="services"></param>
-        public static void ReigsterServices(IServiceCollection services) 
+        public static void ReigsterServices(IServiceCollection services)
         {
+            //Domain InMemoryBus MediatR
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            //Domain Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             //Application Layer
             services.AddScoped<ICourseService, CourseService>();
 
             //Infra.Data Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+
+            services.AddScoped<UniversityDBContext>();
         }
     }
 }
